deacb2ac32786774f5a5aceedb06958ca72a685f
popo1515 <37091634+popo1515@users.noreply.github.com>
2018-03-18 14:27:36 +0000

    Update HelloWorld.java

--- src/homework1/HelloWorld.java
+++ src/homework1/HelloWorld.java
@@ -1,1 +1,6 @@
-public class HelloWorld {    public static void main(String... args) {        //TODO Auto-gennerated method Stub        System.out.println("Hello, World!"+        " Hello, Java!!");    }}
+public class HelloWorld {
+  public static void main(String... args) {
+    //TODO Auto-gennerated method Stub
+    System.out.println("Hello, World!"+" Hello, Java!!");
+  }
+}
