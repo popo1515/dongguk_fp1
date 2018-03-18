3ad9b3f9da1d09d83f6919e3fb29d232cb3d393f
popo1515 <37091634+popo1515@users.noreply.github.com>
2018-03-18 14:24:01 +0000

    Create HelloWorld.java

---
+++ src/homework1/HelloWorld.java
@@ -0,0 +1,1 @@
+public class HelloWorld {    public static void main(String... args) {        //TODO Auto-gennerated method Stub        System.out.println("Hello, World!"+        " Hello, Java!!");    }}
