048bbfbfa1d10b3879e8785fdb3521e61f027705
popo1515 <37091634+popo1515@users.noreply.github.com>
2018-03-18 14:13:42 +0000

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String... args) {
-        //TODO Auto-gennerated method Stub
-        System.out.println("Hello, World!"+
-        " Hello, Java!!");
-    }
-}
